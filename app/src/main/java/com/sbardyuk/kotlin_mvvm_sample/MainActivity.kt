3d028c2566b2ac0d3503661a5450defac58a564a
Stanislav Bardyuk <kodlan@gmail.com>
2019-01-24 15:18:25 +0200

    Android binding

--- app/src/main/java/com/sbardyuk/kotlin_mvvm_sample/MainActivity.kt
+++ app/src/main/java/com/sbardyuk/kotlin_mvvm_sample/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.sbardyuk.kotlin_mvvm_sample
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.sbardyuk.kotlin_mvvm_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.textField.text = "test"
     }
 }
